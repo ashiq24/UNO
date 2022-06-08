45b623635cdde6659f426b90dc62b6ae4dd7e35c
Md Ashiqur rahman <ashiqbuet14@gmail.com>
2022-06-08 00:30:23 -0400

    Update utilities3.py

--- utilities3.py
+++ utilities3.py
@@ -70,34 +70,18 @@ class MatReader(object):
         self.to_float = to_float
 
 
-#loss function with rel/abs Lp loss
+
 class LpLoss(object):
     def __init__(self, d=2, p=2, size_average=True, reduction=True):
         super(LpLoss, self).__init__()
 
-        #Dimension and Lp-norm type are postive
+
         assert d > 0 and p > 0
 
         self.d = d
         self.p = p
         self.reduction = reduction
         self.size_average = size_average
-
-    def abs(self, x, y):
-        num_examples = x.size()[0]
-
-        #Assume uniform mesh
-        h = 1.0 / (x.size()[1] - 1.0)
-
-        all_norms = (h**(self.d/self.p))*torch.norm(x.view(num_examples,-1) - y.view(num_examples,-1), self.p, 1)
-
-        if self.reduction:
-            if self.size_average:
-                return torch.mean(all_norms)
-            else:
-                return torch.sum(all_norms)
-
-        return all_norms
 
     def rel(self, x, y):
         num_examples = x.size()[0]
