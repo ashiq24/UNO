3c94049d066fde5c14b72dca10b82e4b9491ae7f
Md Ashiqur rahman <ashiqbuet14@gmail.com>
2022-12-21 18:24:14 -0500

    Update navier_stokes_uno2d.py

--- navier_stokes_uno2d.py
+++ navier_stokes_uno2d.py
@@ -28,7 +28,7 @@ class UNO_P(nn.Module):
         """
         The overall network. It contains 7 integral operator.
         1. Lift the input to the desire channel dimension by  self.fc, self.fc0 .
-        2. 4 layers of the integral operators u' = (W + K)(u).
+        2. 7 layers of the integral operators u' = (W + K)(u).
             W defined by self.w; K defined by self.conv .
         3. Project from the channel space to the output space by self.fc1 and self.fc2 .
         input: the solution of the first 10 timesteps (u(1), ..., u(10)).
